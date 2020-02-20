c300404e078a96b9823223bb8e2fda227ea8f16a
Amitabh Kumar <amitabh.kumar2@gmail.com>
2020-02-20 10:43:30 -0500

    product deletes on product @0 qty @

--- models/departments.js
+++ models/departments.js
@@ -4,19 +4,8 @@ module.exports = (sequelize,DataTypes) => {
   let Department = sequelize.define('departments', {
     name: DataTypes.STRING,
     overheadCosts: DataTypes.FLOAT,
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-      validate: {
-        isEmail: true
-      }
-    },
-    // The password cannot be null
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
+    email: DataTypes.STRING
+  }, {
     // Model tableName will be the same as the model name instead of being pluralized
     freezeTableName: true
   });
@@ -26,16 +15,6 @@ module.exports = (sequelize,DataTypes) => {
       // Department has a one-to-many relationship with Products
       db.departments.hasMany(db.products,{as: 'Products'})
     }
-
-      // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
-      Department.prototype.validPassword = function(password) {
-    return bcrypt.compareSync(password, this.password);
-  };
-  // Hooks are automatic methods that run during various phases of the User Model lifecycle
-  // In this case, before a User is created, we will automatically hash their password
-  Department.addHook("beforeCreate", function(departments) {
-    Department.password = bcrypt.hashSync(departments.password, bcrypt.genSaltSync(10), null);
-  });
 
   return Department
 }
