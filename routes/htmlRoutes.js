6bacdad0c9704d85dcb317be1b5ea07d20a09cf7
Amitabh Kumar <amitabh.kumar2@gmail.com>
2020-02-22 09:01:19 -0500

    added login logout working in pieces

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,8 +1,35 @@
 
 const path = require('path')
+var isAuthenticated = require("../config/middleware/isAuthenticated");
+
 
 // HTML-Routes
 module.exports = (app) => {
+
+  
+  app.get("/signup", function(req, res) {
+    // If the user already has an account send them to the members page
+    // if (req.user) {
+    //   res.redirect("/");
+    // }
+    res.sendFile(path.join(__dirname, "../public/signup.html"));
+  });
+
+  app.get("/login", function(req, res) {
+    // If the user already has an account send them to the members page
+    // if (req.user) {
+    //   res.redirect("/");
+    // }
+    res.sendFile(path.join(__dirname, "../public/login.html"));
+  });
+
+  // Here we've add our isAuthenticated middleware to this route.
+  // If a user who is not logged in tries to access this route they will be redirected to the signup page
+  app.get("/signup", isAuthenticated, function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
+
+
 
   // Department HTML routes .................................................
   app.get('/depmts', (req, res) => {
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,6 @@ module.exports = (app) => {
   require('./htmlRoutes')(app)
   require('./apiRoutes')(app) 
   require('./api-routes')(app)
-  require('./html-routes')(app) 
+  // require('./html-routes')(app) 
   
 }
