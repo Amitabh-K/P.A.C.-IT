9c201275aaa34042bbfc7295be30c43d98c45415
Amitabh Kumar <amitabh.kumar2@gmail.com>
2020-02-23 12:03:51 -0500

    adding passport n bycypt

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,44 +1,75 @@
+
 const path = require('path')
+var isAuthenticated = require("../config/middleware/isAuthenticated");
+
 
 // HTML-Routes
 module.exports = (app) => {
 
+  
+  app.get("/signup", function(req, res) {
+    // If the user already has an account send them to the members page
+    if (req.user) {
+      res.redirect("/");
+    }
+    res.sendFile(path.join(__dirname, "../public/auth/signup.html"));
+  });
+
+  app.get("/login", function(req, res) {
+    // If the user already has an account send them to the members page
+    if (req.user) {
+      res.redirect("/");
+    }
+    res.sendFile(path.join(__dirname, "../public/auth/login.html"));
+  });
+
+  app.get("/logout", function(req, res) {
+    // If the user already has an account send them to the members page
+
+    res.sendFile(path.join(__dirname, "../public/auth/logout.html"));
+  });
+
+ 
   // Department HTML routes .................................................
   app.get('/depmts', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/departments/index.html'))
   })
 
-  app.get('/depmts-add', (req, res) => {
+  app.get('/depmts-add', isAuthenticated, (req, res) => {
     res.sendFile(path.join(__dirname, '../public/departments/deptAdd.html'))
   })
 
-  app.get('/depmts-edit', (req, res) => {
+  app.get('/depmts-edit', isAuthenticated, (req, res) => {
     res.sendFile(path.join(__dirname, '../public/departments/deptEdit.html'))
   })
 
-  app.get('/depmts-delete', (req, res) => {
+  app.get('/depmts-delete', isAuthenticated, (req, res) => {
     res.sendFile(path.join(__dirname, '../public/departments/deptDelete.html'))
   })
 
   // Products HTML routes.....................................................
-  app.get('/prods', (req, res) => {
+  app.get('/', isAuthenticated, (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+  })
+
+  app.get('/prods', isAuthenticated, (req, res) => {
     res.sendFile(path.join(__dirname, '../public/products/index.html'))
   })
 
-  app.get('/prods-add', (req, res) => {
+  app.get('/prods-add', isAuthenticated,  (req, res) => {
     res.sendFile(path.join(__dirname, '../public/products/prodAdd.html'))
   })
 
-  app.get('/prods-edit', (req, res) => {
+  app.get('/prods-edit', isAuthenticated, (req, res) => {
     res.sendFile(path.join(__dirname, '../public/products/prodEdit.html'))
   })
 
-  app.get('/prods-delete', (req, res) => {
+  app.get('/prods-delete', isAuthenticated, (req, res) => {
     res.sendFile(path.join(__dirname, '../public/products/prodDelete.html'))
   })
 
   // Orders ..................................................................
-  app.get('/carts', (req, res) => {
+  app.get('/carts', isAuthenticated, (req, res) => {
     res.sendFile(path.join(__dirname, '../public/carts/index.html'))
   })
 
